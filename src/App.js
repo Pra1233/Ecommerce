9f05ea13a0e5ac402334c71bf6b78863906a373c
Prabhat <103235389+Pra1233@users.noreply.github.com>
2023-11-21 20:21:37 +0530

    Loading the application

--- src/App.js
+++ src/App.js
@@ -1,16 +1,17 @@
 import React from "react";
 
 import MoviesList from "./components/MoviesList";
+import Load from "./components/Load";
 import "./App.css";
 import { useState } from "react";
 
 function App() {
-
-
   const [movies, setMovies] = useState([]);
+  const [loading, setIsLoading] = useState(false);
 
   async function fetchMoviesHandler() {
     try {
+      setIsLoading(true);
       const res = await fetch("https://swapi.dev/api/films/");
       const data = await res.json();
 
@@ -25,13 +26,18 @@ function App() {
 
       console.log(data);
       setMovies(transformedMovies);
+      setIsLoading(false);
     } catch (e) {
       console.log(e);
     }
   }
 
+  // if (movies) {
+  //   setIsLoading(false);
+  // }
   return (
     <React.Fragment>
+      {loading && <Load />}
       <section>
         <button onClick={fetchMoviesHandler}>Fetch Movies</button>
       </section>
--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
 
-import './index.css';
-import App from './App';
+//React Bootstrap Configuration
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+import "../node_modules/bootstrap/dist/css/bootstrap.css";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(<App />);
